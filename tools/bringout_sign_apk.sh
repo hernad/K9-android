46905e0f4d72fd34aac48369a53afa48baea6c02
hernad <hernad@bring.out.ba>
2017-04-03 17:28:48 +0200

    tools

---
+++ tools/bringout_sign_apk.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+SIGNING_NAME="bring.out doo Sarajevo"
+BUILD_TOOLS_VER=25.0.2
+VALID_DAYS=20000
+
+cd k9mail/build/outputs/apk
+
+if [ ! -f bringout-android.keystore ]
+then
+   echo generating keystore
+   keytool -genkey -v -keystore bringout-android.keystore -alias \
+            "$SIGNING_NAME" -keyalg RSA -keysize 2048 --validity $VALID_DAYS 
+
+fi
+
+jarsigner -verbose -keystore bringout-android.keystore \
+      k9mail-release-unsigned.apk "$SIGNING_NAME"
+
+
+rm k9mail-signed.apk 
+
+$ANDROID_HOME/build-tools/$BUILD_TOOLS_VER/zipalign 4 \
+    k9mail-release-unsigned.apk  k9mail-signed.apk
+
+
+cp k9mail-signed.apk ../../../../
+ 
+cd ../../../../
+ 
+ls -lh k9mail-signed.apk
---
+++ tools/build_only_bosanski.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+./gradlew clean
+
+rm -rf build
+
+LANG=bg
+LANG="$LANG ca"
+LANG="$LANG cs"
+LANG="$LANG da"
+LANG="$LANG de"
+LANG="$LANG el"
+LANG="$LANG es"
+LANG="$LANG et"
+LANG="$LANG eu"
+LANG="$LANG fa"
+LANG="$LANG fi"
+LANG="$LANG fr"
+LANG="$LANG gl"
+LANG="$LANG gl-rES"
+LANG="$LANG hr"
+LANG="$LANG hu"
+LANG="$LANG it"
+LANG="$LANG iw"
+LANG="$LANG ja"
+LANG="$LANG ko"
+LANG="$LANG lt"
+LANG="$LANG lv"
+LANG="$LANG nb"
+LANG="$LANG nl"
+LANG="$LANG pl"
+LANG="$LANG pt-rBR"
+LANG="$LANG pt-rPT"
+LANG="$LANG ro"
+LANG="$LANG ru"
+LANG="$LANG sk"
+LANG="$LANG sl"
+LANG="$LANG sr"
+LANG="$LANG sv"
+LANG="$LANG tr"
+LANG="$LANG uk"
+LANG="$LANG zh-rCN"
+LANG="$LANG zh-rTW"
+
+echo $LANG
+
+echo "brisem sve jezike osim bosanskog"
+
+for lang in $LANG
+do
+   find . -name "values-$lang" -type d -exec  rm -rf {} \;
+done
+
+echo "echo iz bs setujem strings.xml"
+cp ./k9mail/src/main/res/values/strings.xml  .
+cp ./k9mail/src/main/res/values-bs/strings.xml  ./k9mail/src/main/res/values/strings.xml
+
+./gradlew build
+
+mkdir -p k9mail/build/outputs/apk
+cp bringout-android.keystore k9mail/build/outputs/apk/
+
+
+
+echo "uraditi 'git checkout -f' da se vrate jezici"
+
