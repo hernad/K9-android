3dc2b23fe00a8cd1f07b547bfc98a11179959120
hernad <hernad@bring.out.ba>
2018-03-18 21:26:43 +0100

    k9mail-signed.apk

--- tools/build_only_bosanski.sh
+++ tools/build_only_bosanski.sh
@@ -54,7 +54,10 @@ do
 done
 
 echo "echo iz bs setujem strings.xml"
+
 cp ./k9mail/src/main/res/values-bs/strings.xml  ./k9mail/src/main/res/values/strings.xml
+mkdir -p $APK_DIR
+
 
 if [ x$1 == xDEBUG ]
 then
